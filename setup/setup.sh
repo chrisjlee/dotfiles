84ff6f5ed4175ed42f8930759305e4c29ee3549c
chris <chris@cjl.(none)>
2012-08-07 12:16:29 -0500

    Update setup install file as well as other setup scripts

--- setup/setup.sh
+++ setup/setup.sh
@@ -3,11 +3,26 @@ echo "=============================="
 echo "   Setup .dotfiles"
 echo "=============================="
 
-for filename in "$PWD/.bash_*"
+function quit() {
+  unset DOTFILES
+  exit 0
+}
+
+if [ -d $HOME/.dotfiles ]; then
+  DOTFILES="$HOME/.dotfiles"
+else 
+  echo "Please move dotfiles to $DOTFILES/.dotfiles"
+  quit  
+fi
+
+for filename in "$DOTFILES/.bash_*"
 do
-  sudo cp -vf "$filename" $HOME
+  sudo cp -vf $filename $HOME
 done
 
-sudo cp -rv "$PWD/.vim*" $HOME
-sudo cp -v "$PWD/.gitc*" $HOME
-sudo cp -v "$PWD/.giti*" $HOME
+sudo cp -rv $DOTFILES/.vim* $HOME
+sudo cp -v $DOTFILES/.gitc* $HOME
+sudo cp -v $DOTFILES/.giti* $HOME
+
+quit
+
---
+++ setup/ssh-config/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#cat ~/.dotfiles/.ssh/config >> ~/.ssh/config
+
+if [ -f ../.ssh/example.config ]; then
+cat ../.ssh/example.config
+fi
