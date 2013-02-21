83297ac86200a5e1c6873dafc7679e3d237cbd4b
Chris Lee <iam@chrisjlee.net>
2013-02-21 13:44:20 -0600

    Silence the drush clone

--- setup/drush/setup.sh
+++ setup/drush/setup.sh
@@ -4,7 +4,7 @@
 # http://drupalcode.org/project/drush.git/blob_plain/HEAD:/README.txt
 #
 echo "Cloning Drush into $HOME"
-git clone --recursive --branch 7.x-4.x http://git.drupal.org/project/drush.git $HOME/drush
+git clone --recursive --branch 7.x-4.x http://git.drupal.org/project/drush.git $HOME/drush > /dev/null
 sudo mv $HOME/drush /etc/drush
 sudo chmod u+x /etc/drush/drush.php
 echo "Changing Permissions of '/etc/drush/drush.php'"
@@ -16,4 +16,5 @@ Drush Install complete!
 
 Now checkout with git the latest tag
 ==================================================================='
+popd '/etc/drush/'
 exit 0
