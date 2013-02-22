80cfee0cbcd81d185169797172a2715b26f44f99
Chris Lee <iam@chrisjlee.net>
2013-02-22 13:48:05 -0600

    Remove incomplete statement

--- setup.sh
+++ setup.sh
@@ -8,11 +8,10 @@ function quit() {
   exit 0
 }
 
-# Setup dotfiles
 if [ -d $HOME/.dotfiles ]; then
   DOTFILES="$HOME/.dotfiles"
 else
-  echo "[ERROR] Please move dotfiles to $DOTFILES/.dotfiles and rerun setup"
+  echo "Please move dotfiles to $DOTFILES/.dotfiles"
   quit 
 fi
 
@@ -23,36 +22,15 @@ function setup_bash_files() {
   done
 }
 
-# Setup ssh files and copy over templates
-function setup_ssh_files() {
-  if [ ! -f $HOME/.ssh ]; then
-    mkdir "$HOME/.ssh"
-  fi
-  if [ ! -f $HOME/.ssh/config ]; then
-    cp $DOTFILES/config $HOME
-  fi
-  if [ ! -f $HOME/example-config ]; then
-    cp $DOTFILES/example-config $HOME
-  fi
-}
+setup_bash_files
 
 # Copy over vim files
-function setup_vim_files() {
-  cp -rv $DOTFILES/.vim* $HOME
-}
+cp -rv $DOTFILES/.vim* $HOME
 # Copy over gitconfig files
-function setup_git_files () {
-  cp -fv $DOTFILES/.gitc* $HOME
-}
+cp -fv $DOTFILES/.gitc* $HOME
 # Copy over zsh files
-function setup_zsh_files() {
-  cp -fv $DOTFILES/.zsh_* $HOME
-}
+cp -fv $DOTFILES/.zsh_* $HOME
 
-setup_bash_files
-setup_ssh_files
-setup_vim_files
-setup_zsh_files
 
 # Done
 quit
